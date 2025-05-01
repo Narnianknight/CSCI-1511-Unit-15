5cb47e5d1b97ea4877cf8a194fb7584a2eb64015
Narnianknight <83618342+Narnianknight@users.noreply.github.com>
2025-05-01 17:22:42 -0400

    Create plot

--- unit15_jschrock4-1.py
+++ unit15_jschrock4-1.py
@@ -5,3 +5,25 @@ from pathlib import Path
 
 path = Path("OHUR.csv")
 lines = path.read_text(encoding="utf-8").splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+dates = []
+unemployment_rates = []
+
+for row in reader:
+    try:
+        date = datetime.strptime(row[0], "%Y-%m-%d")
+        unemployment_rate = float(row[1])
+    except ValueError as e:
+        print(row)
+    else:
+        dates.append(date)
+        unemployment_rates.append(unemployment_rate)
+
+figure, graph = plt.subplots()
+
+graph.plot(dates, unemployment_rates)
+
+plt.show()
